1a3849e76242cbd21d9f84a7959148250b1166a9
bignya <bignya19@gmail.com>
2025-02-04 13:50:19 +0530

    added prompt changes, voice change

--- autism/chatvoice/chains.py
+++ autism/chatvoice/chains.py
@@ -1,11 +1,11 @@
 from langchain_core.prompts import PromptTemplate
-from prompts import AUTISM_PROMPT
+from prompts import AUTISM_PROMPT_1
 from langchain_core.output_parsers import StrOutputParser
 
 def conversation_chain(llm):
     """To get the Conversation Stage"""
     prompt = PromptTemplate(
-        template= AUTISM_PROMPT,
+        template= AUTISM_PROMPT_1,
         input_variables= [
                 "name",
                 "age",
