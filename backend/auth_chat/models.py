7001b45878bbff5bc54782fa7b13ebb52183a0c1
bignya <bignya19@gmail.com>
2025-02-07 23:30:12 +0530

    add username in required

--- backend/auth_chat/models.py
+++ backend/auth_chat/models.py
@@ -17,7 +17,7 @@ class User(AbstractUser):
     disease_level = models.CharField(max_length=20, null=True, blank=True)
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']  # Remove username from required fields
+    REQUIRED_FIELDS = ['name', 'username']  # Remove username from required fields
 
     def __str__(self):
         return self.email
