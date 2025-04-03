f734dd14dd8b98b3bd49cceacf4b7f4d2d7a3159
bignya <bignya19@gmail.com>
2025-04-03 22:53:17 +0530

    some changes

--- backend/chatroom/models.py
+++ backend/chatroom/models.py
@@ -6,10 +6,14 @@ class ChatMessage(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    disease_type = models.CharField(max_length=50)  # Store the disease type with the message
+    disease_type = models.CharField(max_length=50)  
     
     class Meta:
         ordering = ['timestamp']
     
     def __str__(self):
         return f'{self.sender.email} ({self.disease_type}): {self.content[:50]}'
+
+
+
+
